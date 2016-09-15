0aafbc06e08f61b1f0f48d439d9485667d95454e
nazaretl <nazaretl@hu-berlin.de>
2016-09-15 17:05:08 +0200

    2nd basket

--- MSEex0805_RUN/MSEex0805.R
+++ MSEex0805_RUN/MSEex0805.R
@@ -6,10 +6,8 @@ das <- read.csv("data_DAX091222-111222.csv")
 
 d <- seq(from = as.Date("2009-12-22"), to = as.Date("2011-12-21"), length.out = 514)
 
-plot(das ~ d, xlab = "Time", ylab = "DAX30 Index", pch = 20, cex = 1, cex.axis = 1.2, cex.lab = 1.5, lab = c(3, 3, 0), main = "", 
-    col = "blue4") 
+plot(das ~ d, xlab = "Time", ylab = "DAX30 Index", pch = 20, cex = 1, cex.axis = 1.2, cex.lab = 1.5, lab = c(3, 3, 0), main = "", col = "blue4") 
 
 
-plot(das ~ d) #, xlab = "Time", ylab = "DAX30 Index", pch = 20, cex = 1, cex.axis = 1.2, cex.lab = 1.5, lab = c(3, 3, 0), main = "", 
-     col = "blue4") 
+plot(das ~ d, xlab = "Time", ylab = "DAX30 Index", pch = 20, cex = 1, cex.axis = 1.2, cex.lab = 1.5, lab = c(3, 3, 0), main = "",  col = "blue4") 
 
