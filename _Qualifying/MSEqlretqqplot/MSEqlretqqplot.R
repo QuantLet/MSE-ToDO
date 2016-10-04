fc17e5193b5dc67da6c37eafb4f97721e86692d6
nazaretl <nazaretl@hu-berlin.de>
2016-10-04 12:24:33 +0200

    last changes

--- _Qualifying/MSEqlretqqplot/MSEqlretqqplot.R
+++ _Qualifying/MSEqlretqqplot/MSEqlretqqplot.R
@@ -26,5 +26,4 @@ mtext(side = 3, text = "Normal Q-Q plot", line = 1, cex = 2.8)
 mtext(side = 1, text = "Theoretical Quantiles", line = 4, cex = 2.5)
 mtext(side = 2, text = "Sample Quantiles", line = 4, cex = 2.5)
 axis(side = 2, at = seq(-0.4, 0.3, by = 0.05), label = seq(-0.4, 0.3, by = 0.05), lwd = 1.2, cex.axis = 2.5, lwd.ticks = 2.5)
-axis(side = 1, at = seq(-3, 3, by = 1), label = seq(-3, 3, by = 1), lwd = 1, cex.axis = 2.5, padj = 0.5, lwd.ticks = 2.5)
- 
+axis(side = 1, at = seq(-3, 3, by = 1), label = seq(-3, 3, by = 1), lwd = 1, cex.axis = 2.5, padj = 0.5, lwd.ticks = 2.5) 
